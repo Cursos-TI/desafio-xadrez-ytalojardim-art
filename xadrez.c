8d3f5507512ab3a4863e3ad87723bb52e2dd6ccb
ytalojardim-art <ytalojardim@gmail.com>
2025-08-23 00:04:56 +0000

    Implementação nivel aventureiro com algumas melhorias.

--- xadrez.c
+++ xadrez.c
@@ -30,99 +30,162 @@ int main() {
 
     int peca = 0;       // variavel para armazenar qual a peça o usuario quer movimentar.
     int movimento = 0;  // variavel para armazenar quantos movimento o usuario quer fazer.
-    int direcao=0;      // variavel para armazenar qual a direção o usuario quer fazer.
+    int direcao=0, direcao2 =0;      // variavel para armazenar qual a direção o usuario quer fazer.
 
-    printf("Escolha a peça para movimentar\n\n");   // menu para escolher qual peça que rmovimentar
+    printf("escolha a peça para movimentar?\n\n");   // menu para escolher qual peça que rmovimentar
     printf("1 Torre\n");
     printf("2 Bispo\n");
     printf("3 Rainha\n");
+    printf("4 Cavalo\n");
     scanf("%d", &peca);
 
-    printf("Quantas casas deseja mover\n\n"); // escolher quantas casas deseja mover
-    scanf("%d", &movimento);
+    int movimento2 = 0;
 
-    if(peca == 2) {
-        printf("Para qual lado deseja mover\n\n"); // usei if esle porque o movimento do bispo é diferente do movimento das demais peças
-        printf("1 Cima, Direita\n");
-        printf("2 Cima, Esquerda\n");
-        printf("3 Baixo, Direita \n");           // escolher a direção que deseja mover o bispo.
-        printf("4 Baixo, Esquerda\n");
+
+
+
+
+
+    if(peca == 2) {                                                 // if-else para imprimir o menu de cada peça pois os movimentos sao distintos
+        printf("!!Digite quantas casas deseja mover!!!\n");
+        scanf("%d", &movimento);
+
+        printf("para qual lado deseja mover?\n\n"); // usei if esle porque o movimento do bispo é diferente do movimento das demais peças
+        printf("1 cima , direita \n");
+        printf("2 cima, esquerda \n");
+        printf("3 baixo, direita  \n");           // escolher a direção que deseja mover o bispo.
+        printf("4 baixo, esquerda  \n");
         scanf("%d", &direcao);
 
-    } else {
-        printf("Para qual lado deseja mover\n\n"); //escolher a direção que deseja mover as demais peças.
-        printf("1 Cima\n");
-        printf("2 Baixo\n");
-        printf("3 Direita\n");
-        printf("4 Esquerda\n");
+    } else if (peca==4) {
+
+        do {
+            printf("Escolha qual direção a peça vai fazer 2 movimentos\n\n"); //escolher a direção que deseja mover o cavalo.
+            printf("1 cima \n");
+            printf("2 baixo \n");
+            printf("3 direita \n");
+            printf("4 esquerda \n");
+            scanf("%d", &direcao);
+        } while(direcao < 1 || direcao > 4);
+
+        do {
+            printf("Escolha qual direção a peça vai fazer 1 movimentos\n\n");
+            printf("1 cima \n");
+            printf("2 baixo \n");
+            printf("3 direita \n");
+            printf("4 esquerda \n");
+            scanf("%d", &direcao2);
+        } while(direcao2 < 1 || direcao2 > 4);
+    }  else {
+        printf("!!Digite quantas casas deseja mover!!!\n");
+        scanf("%d", &movimento);
+
+        printf("para qual lado deseja mover?\n\n"); //escolher a direção que deseja mover as demais peças.
+        printf("1 cima\n");
+        printf("2 baixo\n");
+        printf("3 direita\n");
+        printf("4 esquerda\n");
         scanf("%d", &direcao);
     }
+
     switch(peca) {                          // usei switch porque precisara escolher varias peças diferentes.
 
     case 1: {
-        printf("Torre\n\n");
+        printf("torre\n\n");
         for(int torre = 1; torre <=movimento; torre++) {        // for para fazer a repetição de movimentos
             if(direcao == 1) {
-                printf("Cima %d\n", torre);
-            }else if(direcao == 2){
-                printf("Baixo %d\n", torre);
-            }else if(direcao == 3){
-                printf("Direita %d\n", torre);
-            }else{
-                printf("Esquerda %d\n", torre);
+                printf("cima %d\n", torre);
+            } else if(direcao == 2) {
+                printf("baixo %d\n", torre);
+            } else if(direcao == 3) {
+                printf("direita %d\n", torre);
+            } else {
+                printf("esquerda %d\n", torre);
             }
         }
-
         break;
-        case 2: {
+    }
+    case 2: {
 
         int bispo=1;
 
-            printf("Bispo\n\n");
-            while(bispo<=movimento) {               // while como estrutura de repetição achei mais viavel para bispo
-
-            if(direcao == 1){
-                printf("Cima, Direita %d\n", bispo);
-            }else if(direcao == 2){
-                printf("Cima, Esquerda %d\n", bispo);
-            }else if(direcao == 3){
-                printf("Baixo, Direita %d\n", bispo);
-            }else{
-                printf("Baixo, Esquerda %d\n", bispo);
-            }
-                bispo++;
-            }
-        }
-
-        break;
-        case 3: {
-
-            int rainha=1;
-
-            printf("Rainha\n\n");
-            do {                                     // DO While como estrutura de repetição achei mais viavel para rainha
+        printf("Bispo\n\n");
+        while(bispo<=movimento) {               // while como estrutura de repetição achei mais viavel para bispo
 
             if(direcao == 1) {
-                printf("Cima %d\n", rainha);
-            }else if(direcao == 2){
-                printf("Baixo %d\n", rainha);
-            }else if(direcao == 3){
-                printf("Direita %d\n", rainha);
-            }else{
-                printf("Esquerda %d\n", rainha);
+                printf("cima, direita %d\n", bispo);
+            } else if(direcao == 2) {
+                printf("cima, esquerda %d\n", bispo);
+            } else if(direcao == 3) {
+                printf("baixo, direita %d\n", bispo);
+            } else {
+                printf("baixo, esquerda %d\n", bispo);
             }
-                rainha++;
+            bispo++;
+        }
+    }
 
-            } while(rainha<=movimento);
+    break;
+    case 3: {
 
+        int rainha=1;
+
+        printf("Rainha\n\n");
+        do {                                     // DO While como estrutura de repetição achei mais viavel para rainha
+
+            if(direcao == 1) {
+                printf("cima %d\n", rainha);
+            } else if(direcao == 2) {
+                printf("baixo %d\n", rainha);
+            } else if(direcao == 3) {
+                printf("direita %d\n", rainha);
+            } else {
+                printf("esquerda %d\n", rainha);
+            }
+            rainha++;
+        } while (rainha <= movimento );
+    }
+
+    break;
+    case 4: {
+    int movimento_cavalo = 2;  // variavel para definir primeiro for como 2 porque nao conseguir colocar para escolher movimento, mesmo pesquisando.
+    int movimento_cavalo2 = 1; // variavel para definir segundo for como 1 porque nao conseguir colocar para escolher movimento, mesmo pesquisando.
+
+        for(int cavalo2=1; cavalo2 <= movimento_cavalo2; cavalo2++) {       // for aninhado para imprimir as retições das duas direções do cavalo
+            for (int cavalo = 1; cavalo <= movimento_cavalo; cavalo++) {
+                if (direcao == 1) {
+                    printf("Cima %d \n", cavalo);       // if-else para depois de escolher a direçao, imprimir as retiçoes na direção escolhida
+                } else if(direcao == 2) {
+                    printf("Baixo %d \n", cavalo);
+                } else if(direcao==3) {
+                    printf("Direita %d  \n", cavalo);
+                } else {
+                    printf("Esquerda %d \n", cavalo);
+                }
+
+            }
+            if(direcao2 == 1) {
+                printf("Cima %d \n", cavalo2 );          // if-else para depois de escolher a direçao, imprimir as repetições na escolhida na direção 2.
+            } else if(direcao2 == 2) {
+                printf("Baixo %d \n", cavalo2 );
+            } else if (direcao2 == 3) {
+                printf("Direita %d \n", cavalo2 );
+            } else {
+                printf("Esquerda %d \n", cavalo2);
+            }
 
         }
         break;
-        default:{
-            printf("Opção invalida");
-        }
     }
+
+
+    default: {
+        printf("Opção invalida");
+        break;
     }
-        
+
+    }
+
+      
   return 0;
 }
